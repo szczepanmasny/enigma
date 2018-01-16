415494ed591ce3e94a74af8f83e08064be35af14
Szczepan Masny <szczepanmasny@gmail.com>
2018-01-16 22:43:32 +0100

    vuex

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 
 let actions = {
-    // getPermission({commit}, id) {
-    //   Vue.http.get(prefix + 'permissions/' + id)
-    //     .then((response) => commit('permission', response.body))
-    // },
+    //
 }
 
 export default actions
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,13 +1,5 @@
 let mutations = {
-  openProfileDialog(state) {
-    state.profileDialog = true
-  },
-  openHelpDialog(state) {
-    state.helpDialog = true
-  },
-  permission(state, data) {
-    state.permission = data;
-  },
+  //
 }
 
 export default mutations
